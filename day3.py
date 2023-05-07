1854c880062de8ec5d2eb3f1ec8284e8b924b020
sarah-zhan <yizh.contact@gmail.com>
2023-05-07 14:08:31 -0700

    multiple if

--- day3.py
+++ day3.py
@@ -1,15 +1,26 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-
+bill = 0
 if height >= 120:
   print('You can ride the rollercoaster!')
   age = int(input('What is your age?'))
   if age < 12:
-     print('Please pay $5.')
+     bill = 5
+     print(f'Child tickets are ${bill}.')
   elif age <= 18:
-     print('Please pay $7.')
+     bill = 7
+     print(f'Youth tickets are ${bill}.')
   else:
-     print('Please pay $12.')
+     bill = 12
+     print(f'Adult tickets are ${bill}.')
+
+  wants_photo = input('Do you want a photo taken? Y or N.')
+  if (wants_photo == 'Y' or wants_photo =='y'):
+     #add $3
+     bill += 3
+
+  print(f'Your total is ${bill}')
+
 else:
   print('Sorry, you need to be taller.')
 
