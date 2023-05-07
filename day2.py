f15e45ea8fddf83b1510dee5929a413dc147a7cc
sarah-zhan <yizh.contact@gmail.com>
2023-05-07 11:11:31 -0700

    life in weeks

--- day2.py
+++ day2.py
@@ -14,3 +14,15 @@ weight = input("enter your weight in kg: ")
 #Write your code below this line 👇
 BMI = float(weight) /  (float(height) * float(height))
 print(int(BMI))
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+years = 90 - int(age)
+months = years * 12
+weeks = years * 52
+days = years * 365
+
+print(f'You have {days} days, {weeks} weeks, and {months} months left.')
