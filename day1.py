0bcfc51b5904d2fbcf939a027e935ce070bf69f9
sarah-zhan <yizh.contact@gmail.com>
2023-05-07 11:07:56 -0700

    variables

--- day1.py
+++ day1.py
@@ -9,3 +9,23 @@ print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
 print(len(input("What is your name? ")))
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a
+a = b
+b = c
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
