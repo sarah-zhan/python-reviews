cde08644c1b912c67e02a0cd6d35683e1b8cf368
sarah-zhan <yizh.contact@gmail.com>
2023-05-07 11:33:14 -0700

    odd or even number

--- day3.py
+++ day3.py
@@ -5,3 +5,15 @@ if height > 120:
   print('You can ride the rollercoaster!')
 else:
   print('Sorry, you need to be taller.')
+
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if (number % 2 == 0):
+    print('This is an even number')
+else:
+    print('This is an odd number.')
