92cfa706002a966817f4f395d48064765a0e6389
sarah-zhan <yizh.contact@gmail.com>
2023-05-07 11:58:29 -0700

    round the number

--- day3.py
+++ day3.py
@@ -32,15 +32,15 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = weight / height **2
+bmi = round(weight / height ** 2)
 
 if bmi < 18.5:
-   print('You are underweight.')
+   print(f'Your bmi is {bmi}. You are underweight.')
 elif bmi < 25:
-   print('You have a normal weight.')
+   print(f'Your bmi is {bmi}. You have a normal weight.')
 elif bmi < 30:
-   print('You are slightly overweight.')
+   print(f'Your bmi is {bmi}. You are slightly overweight.')
 elif bmi < 35:
-   print('You are obese.')
+   print(f'Your bmi is {bmi}. You are obese.')
 else:
-   print('You are clinically obese.')
+   print(f'Your bmi is {bmi}. You are clinically obese.')
