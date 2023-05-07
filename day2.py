2138c3c46ad0661d3dcb6376c2eb24c3517ae404
sarah-zhan <yizh.contact@gmail.com>
2023-05-07 11:11:02 -0700

    BMI Calculator

--- day2.py
+++ day2.py
@@ -5,3 +5,12 @@ two_digit_number = input("Type a two digit number: ")
 ####################################
 #Write your code below this line 👇
 print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+BMI = float(weight) /  (float(height) * float(height))
+print(int(BMI))
