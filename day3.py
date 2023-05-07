a8189965d5d1fedcd1b19e5e641d7e692ba765f5
sarah-zhan <yizh.contact@gmail.com>
2023-05-07 11:52:31 -0700

    BMI 2.0

--- day3.py
+++ day3.py
@@ -24,3 +24,23 @@ if (number % 2 == 0):
     print('This is an even number')
 else:
     print('This is an odd number.')
+
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = weight / height **2
+
+if bmi < 18.5:
+   print('You are underweight.')
+elif bmi < 25:
+   print('You have a normal weight.')
+elif bmi < 30:
+   print('You are slightly overweight.')
+elif bmi < 35:
+   print('You are obese.')
+else:
+   print('You are clinically obese.')
