2f5fc7b64c3c308c2a743e3b5a0a82c4afb022e2
sarah-zhan <yizh.contact@gmail.com>
2023-05-07 11:12:16 -0700

    tip calculator

--- day2.py
+++ day2.py
@@ -26,3 +26,18 @@ weeks = years * 52
 days = years * 365
 
 print(f'You have {days} days, {weeks} weeks, and {months} months left.')
+
+#If the bill was $150.00, split between 5 people, with 12% tip.
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+print('welcome to the tip calculator')
+total = float(input('what is the total bill before tip? $'))
+people = int(input('how many people to split the bill? '))
+tip = float(input('how many tip to pay 10, 12, or 15 ')) * 0.01
+pay_by_person = (total / people) * (1 + tip)
+print(f'Everyone pay {pay_by_person:.2f}')
