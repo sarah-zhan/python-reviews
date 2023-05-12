98cce450dda81f5951bf047c7bf63a7b9e1f6895
sarah-zhan <yizh.contact@gmail.com>
2023-05-11 19:07:14 -0700

    append the result to the list

--- day7.py
+++ day7.py
@@ -3,13 +3,20 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+#Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = word_list[random.randint(0, 2)]  # or random.choice(world_list)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+#Testing code
+print(f'The solution is {chosen_word}.')
+
+#Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess_letter = input('Guess of a letter: ').lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+#create a list
+display = []
+#Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 for letter in chosen_word:
     if letter == guess_letter:
-        print('Right')
+        display.append(letter)
     else:
-        print('Wrong')
+        display.append('_')
