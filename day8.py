be1c5c60f96354bba83f2625a0a7a2f4b2c59bfc
sarah-zhan <yizh.contact@gmail.com>
2023-05-13 00:24:52 -0700

    check prime number

--- day8.py
+++ day8.py
@@ -17,7 +17,7 @@ import math
 def paint_calc(height, width, cover):
     numbs = math.ceil(height * width / cover)
     print(f"You'll need {numbs} cans of paint.")
-    
+
 #Write your code above this line 👆
 # Define a function called paint_calc() so that the code below works.
 
@@ -26,3 +26,22 @@ test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
+#Write your code below this line 👇
+def prime_checker(number):
+    result_list = []
+    for n in range(1, number + 1):
+        result = number % n
+        if result == 0:
+            result_list.append(n)
+    if len(result_list) == 2 and 1 in result_list and number in result_list:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+#Write your code above this line 👆
+
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
