b0c7ac1b8ed6b2c1a49b3c017f12af7a0f21f0d8
sarah-zhan <yizh.contact@gmail.com>
2023-05-14 06:36:38 -0700

    refactor

--- day9.py
+++ day9.py
@@ -9,15 +9,17 @@ student_scores = {
 #Create an empty dictionary called student_grades.
 student_grades = {}
 
-#Write your code below to add the grades to student_grades.
+#add the grades to student_grades.
 for name in student_scores:
-  if student_scores[name] > 90:
+  score = student_scores[name]
+  if score > 90:
     student_grades[name] = 'Outstanding'
-  elif student_scores[name] > 80:
+  elif score > 80:
     student_grades[name] = 'Exceeds Expectations'
-  elif student_scores[name] > 70:
+  elif score > 70:
     student_grades[name] = 'Acceptable'
   else:
     student_grades[name] = 'Fail'
 
 print(student_grades)
+
