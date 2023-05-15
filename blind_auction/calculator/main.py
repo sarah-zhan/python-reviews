8cf23f6069d5f8a1eda4935fb1909f1fe7e6df27
sarah-zhan <yizh.contact@gmail.com>
2023-05-15 16:09:20 -0700

    define calculator

--- blind_auction/calculator/main.py
+++ blind_auction/calculator/main.py
@@ -1,3 +1,5 @@
+from art import logo
+
 def add(n1, n2):
   return n1 + n2
 
@@ -17,5 +19,33 @@ operations = {
   '/': divide
 }
 
-num1 = int(input("What's the first number? "))
-num2 = int(input("What's the second number? "))
+
+def calculator():
+  print(logo)
+
+  game = True
+  num1 = float(input("What's the first number? "))
+
+  while game:
+    for operation in operations:
+      print(operation)
+
+    operation_symbol = input('Pick an operation from the line above: ')
+
+    num2 = float(input("What's the next number? "))
+
+    calculation = operations[operation_symbol]
+    answer = calculation(num1, num2)
+
+    print(f'{num1} {operation_symbol} {num2} = {answer}')
+
+    game_continued = input("Do you want to continue the calculation? Type 'y' to continue, 'n' to start a new calculation ")
+    if game_continued == 'y':
+      num1 = answer
+    elif game_continued == 'n':
+      game = False
+      calculator()
+    else:
+      print('Not a correct operator')
+
+calculator()
