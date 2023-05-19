47555059ddf6341a02177080886a2d0eab3afa54
sarah-zhan <yizh.contact@gmail.com>
2023-05-19 10:42:28 -0700

    add if 2 options are the same

--- higher_lower/main.py
+++ higher_lower/main.py
@@ -2,15 +2,11 @@ from art import logo, vs
 from data import data
 import random
 
-#choose A function
-# def choose_a():
-#   option_a = random.choice(data)
-#   return f"Compare A: {option_a['name']}, {option_a['description']}, from {option_a['country']}."
-
-#choose B function
-# def choose_b():
-#   option_b = random.choice(data)
-#   return f"Against B: {option_b['name']}, {option_b['description']}, from {option_b['country']}."
+#get random choice
+option_a = random.choice(data)
+option_b = random.choice(data)
+if option_a == option_b:
+  option_b = random.choice(data)
 
 #compare
 def compare(num1, num2):
@@ -19,18 +15,29 @@ def compare(num1, num2):
   elif num1 < num2:
     return 'B'
 
+#check answer
+def check_answer(answer, correct_answer, score):
+  if answer == correct_answer:
+    score += 1
+    print(f"You're right! Current score: {score}")
+    option_a = option_b
+    print(f"Compare A: {option_a['name']}, {option_a['description']}, from {option_a['country']}.")
+    print(vs)
+    option_b = random.choice(data)
+    if option_a == option_b:
+      option_b = random.choice(data)
+    print(f"Against B: {option_b['name']}, {option_b['description']}, from {option_b['country']}.")
+    answer = input("Who has more followers? Type'A' or 'B': ")
+  else:
+    print(f"Sorry, that's wrong. Final score: {score}")
 
 
 #game begin
 print(logo)
 
-option_a = random.choice(data)
-option_b = random.choice(data)
-
-def game_continue():
-  print(f"Compare A: {option_a['name']}, {option_a['description']}, from {option_a['country']}.")
-  print(vs)
-  print(f"Against B: {option_b['name']}, {option_b['description']}, from {option_b['country']}.")
+print(f"Compare A: {option_a['name']}, {option_a['description']}, from {option_a['country']}.")
+print(vs)
+print(f"Against B: {option_b['name']}, {option_b['description']}, from {option_b['country']}.")
 
 #ask user
 answer = input("Who has more followers? Type'A' or 'B': ")
@@ -38,11 +45,6 @@ answer = input("Who has more followers? Type'A' or 'B': ")
 #track the score
 score = 0
 
-#compare user answer and the truth
-if compare(option_a['follower_count'], option_b['follower_count']) == answer:
-  score += 1
-  print(f"You're right! Current score: {score}")
-  option_a = option_b
-  game_continue()
-else:
-  print(f"Sorry, that's wrong. Final score: {score}")
+#compare user answer and the correct answer
+correct_answer = compare(option_a['follower_count'], option_b['follower_count'])
+check_answer(answer, correct_answer, score)
