7e493948b94e7214b93eb92b23af3041db321cef
sarah-zhan <yizh.contact@gmail.com>
2023-05-19 16:24:39 -0700

    need to track the revenue

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -15,36 +15,39 @@ def make_coffee():
   water = resources["water"] - MENU[my_coffee]["ingredients"]["water"]
   milk = resources["milk"] - MENU[my_coffee]["ingredients"]["milk"]
   coffee = resources["coffee"] - MENU[my_coffee]["ingredients"]["coffee"]
-
+  revenue = 0
   #show report
   if my_coffee == 'report':
-    print(f"Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g")
-
-  #check whether machine has enough resources
-  if water < 0:
-    print("There is not enough water.")
-    make_coffee()
-  elif milk < 0:
-    print("There is not enough milk.")
-    make_coffee()
-  elif coffee < 0:
-    print("There is not enough coffee.")
+    print(f"Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${revenue}")
     make_coffee()
   else:
-    print("Please insert coins.")
-    quarter = int(input("How many quarters?: "))
-    dime = int(input("How many dimes?: "))
-    nickle = int(input("How many nickles?: "))
-    penny = int(input("How many pennies?: "))
-    total = calculate_money(quarter, dime, nickle, penny)
-    cost = MENU[my_coffee]["cost"]
-    if total >= cost:
-      change = round(total - cost, 2)
-      print(f"Here is ${change} in change.")
-      print(f"Here is your {my_coffee} ☕. Enjoy!")
+    #check whether machine has enough resources
+    if water < 0:
+      print("There is not enough water.")
+      make_coffee()
+    elif milk < 0:
+      print("There is not enough milk.")
+      make_coffee()
+    elif coffee < 0:
+      print("There is not enough coffee.")
       make_coffee()
     else:
-      print("Sorry, there is not enough money. Money refunded.")
-      make_coffee()
+      print("Please insert coins.")
+      quarter = int(input("How many quarters?: "))
+      dime = int(input("How many dimes?: "))
+      nickle = int(input("How many nickles?: "))
+      penny = int(input("How many pennies?: "))
+      total = calculate_money(quarter, dime, nickle, penny)
+      cost = MENU[my_coffee]["cost"]
+
+      if total >= cost:
+        change = round(total - cost, 2)
+        print(f"Here is ${change} in change.")
+        print(f"Here is your {my_coffee} ☕. Enjoy!")
+        revenue += cost
+        make_coffee()
+      else:
+        print("Sorry, there is not enough money. Money refunded.")
+        make_coffee()
 
 make_coffee()
