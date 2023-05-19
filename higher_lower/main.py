b0e4d570bdd3710e5db3cba6194cdbbd42e3b898
sarah-zhan <yizh.contact@gmail.com>
2023-05-18 21:39:54 -0700

    create 3 functions

--- higher_lower/main.py
+++ higher_lower/main.py
@@ -1,3 +1,21 @@
-from art import logo
+from art import logo, vs
 from data import data
+import random
+
+#choose A function
+def choose_a():
+  option_a = random.choice(data)
+  return f"Compare A: {option_a['name']}, {option_a['description']}, from {option_a['country']}."
+
+#choose B function
+def choose_b():
+  option_b = random.choice(data)
+  return f"Against B: {option_b['name']}, {option_b['description']}, from {option_b['country']}."
+
+#compare
+def compare(num1, num2):
+  if num1 > num2:
+    return 'A'
+  elif num1 < num2:
+    return 'B'
 
