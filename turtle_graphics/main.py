4988fe5422d3366fd14688d9f7f4ea3a734d43ff
sarah-zhan <yizh.contact@gmail.com>
2023-05-22 16:58:09 -0700

    change the turtle color

--- turtle_graphics/main.py
+++ turtle_graphics/main.py
@@ -3,8 +3,9 @@ from turtle import Turtle, Screen
 timmy = Turtle()
 print(timmy)
 timmy.shape("turtle")
-
+timmy.color("gold")
 my_screen = Screen()
+
 print(my_screen.canvheight)
 
 my_screen.exitonclick()
