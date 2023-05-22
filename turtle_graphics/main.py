60324755c1d37de7e51ba4defe0441b613b426ea
sarah-zhan <yizh.contact@gmail.com>
2023-05-22 16:52:47 -0700

    change the shape to turtle

--- turtle_graphics/main.py
+++ turtle_graphics/main.py
@@ -1,1 +1,10 @@
-import turtle 
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
