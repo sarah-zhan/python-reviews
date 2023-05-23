8db5f86df93bde622f2bd6372fda4579747200d1
sarah-zhan <yizh.contact@gmail.com>
2023-05-22 22:40:16 -0700

    create instance of menu and check whether the drink is valid

--- oop-coffee-machine-start/oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -1,3 +1,10 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# create instance of menu
+coffee_menu = Menu()
+my_coffee = input(f"What would you like? ({coffee_menu.get_items()})")
+
+# check whether the choice is valid
+coffee_menu.find_drink(my_coffee)
