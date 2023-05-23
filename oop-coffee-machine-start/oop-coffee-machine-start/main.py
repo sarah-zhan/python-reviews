ade7310a6ee0cf277416d85af23cd54eb1c4420d
sarah-zhan <yizh.contact@gmail.com>
2023-05-22 22:47:43 -0700

    print report if the input is "report"

--- oop-coffee-machine-start/oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -6,5 +6,12 @@ from money_machine import MoneyMachine
 coffee_menu = Menu()
 my_coffee = input(f"What would you like? ({coffee_menu.get_items()})")
 
-# check whether the choice is valid
-coffee_menu.find_drink(my_coffee)
+# MyCoffeeMaker
+my_coffee_maker = CoffeeMaker()
+# print report
+if my_coffee == "report":
+    my_coffee_maker.report()
+elif my_coffee not in (["latte", "espresso", "cappuccino"]):
+    # check whether the choice is valid
+    coffee_menu.find_drink(my_coffee)
+
