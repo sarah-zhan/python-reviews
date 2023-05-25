8c891f1a3ee147dada5d4f2e5e7895a9b80d1334
sarah-zhan <yizh.contact@gmail.com>
2023-05-24 20:34:50 -0700

    fix the prompt bug, question number

--- quiz-game/quiz_brain.py
+++ quiz-game/quiz_brain.py
@@ -4,7 +4,8 @@ class QuizBrain:
         self.question_list = questions
 
     def next_question(self):
-        user_answer = input(f"Question {self.question_number}: {self.question_list[self.question_number].text} "
+        question_number_show = self.question_number + 1
+        user_answer = input(f"Question {question_number_show}: {self.question_list[self.question_number].text} "
                             f"(True or False?)")
         return user_answer
 
