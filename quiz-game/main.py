e930b8aa160d90bb0c0db664467a6c1f0d6ad2ba
sarah-zhan <yizh.contact@gmail.com>
2023-05-24 22:19:53 -0700

    change the question_data

--- quiz-game/main.py
+++ quiz-game/main.py
@@ -5,7 +5,7 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for data in question_data:
-    question_bank.append(Question(data["text"], data["answer"]))
+    question_bank.append(Question(data["question"], data["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
