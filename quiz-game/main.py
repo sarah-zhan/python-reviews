e87e4a181e713afbb4a17e15e69b112aa0c1d089
sarah-zhan <yizh.contact@gmail.com>
2023-05-24 20:27:26 -0700

    create QuizBrain class method next_question

--- quiz-game/main.py
+++ quiz-game/main.py
@@ -1,10 +1,11 @@
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 # create question bank
 question_bank = []
 
 for data in question_data:
     question_bank.append(Question(data["text"], data["answer"]))
 
-
+quiz = QuizBrain(question_bank)
+quiz.next_question()
--- quiz-game/quiz_brain.py
+++ quiz-game/quiz_brain.py
@@ -2,3 +2,11 @@ class QuizBrain:
     def __init__(self, questions):
         self.question_number = 0
         self.question_list = questions
+
+    def next_question(self):
+        user_answer = input(f"Question {self.question_number}: {self.question_list[self.question_number].text} "
+                            f"(True or False?)")
+        return user_answer
+
+
+
