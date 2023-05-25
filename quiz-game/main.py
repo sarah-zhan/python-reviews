70917aca795e49247f77d40fb1937888053b84b4
sarah-zhan <yizh.contact@gmail.com>
2023-05-24 17:30:43 -0700

    create Question bank

---
+++ quiz-game/main.py
@@ -0,0 +1,9 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for data in question_data:
+    question_bank.append(Question(data["text"], data["answer"]))
+
+
