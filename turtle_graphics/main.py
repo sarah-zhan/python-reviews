312deb7fee5e97ab3b1f379084f8d658ad3ce1ea
sarah-zhan <yizh.contact@gmail.com>
2023-05-25 20:47:52 -0700

    draw 8 different shapes with different colors

--- turtle_graphics/main.py
+++ turtle_graphics/main.py
@@ -1,7 +1,6 @@
 from turtle import Turtle, Screen
-
+import random
 timmy = Turtle()
-print(timmy)
 timmy.shape("turtle")
 timmy.color("black")
 
@@ -15,16 +14,28 @@ timmy.color("black")
 # timmy.forward(100)
 
 # draw a dash line
-for _ in range(10):
-    timmy.pendown()
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
+# for _ in range(10):
+#     timmy.pendown()
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+
+my_screen = Screen()
+
+# Setting the screen color-mode
+my_screen.colormode(255)
+# draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, decagon (3-10 sides)
+for number in range(3, 11):
+    r = random.randint(0, 255)
+    b = random.randint(0, 255)
+    g = random.randint(0, 255)
+    timmy.pencolor(r, g, b)
+    for _ in range(number):
+        timmy.forward(100)
+        timmy.right(360/number)
 
 
-
-
-
+my_screen.exitonclick()
 
 my_screen = Screen()
 my_screen.exitonclick()
