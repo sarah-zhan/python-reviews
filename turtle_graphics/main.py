94637d50e896a7dee0a1a9a6c2b1c894c0d50eff
sarah-zhan <yizh.contact@gmail.com>
2023-05-25 17:18:50 -0700

    draw a square

--- turtle_graphics/main.py
+++ turtle_graphics/main.py
@@ -1,20 +1,24 @@
-# from turtle import Turtle, Screen
-#
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("gold")
-# timmy.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-#
-# my_screen.exitonclick()
+from turtle import Turtle, Screen
 
-from prettytable import PrettyTable
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("gold")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
 
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-table.align = "l"
-print(table)
+my_screen = Screen()
+my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+#
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = "l"
+# print(table)
