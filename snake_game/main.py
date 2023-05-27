16589d6ec99bb7a2dfa2cef295e1f5fc87e86476
sarah-zhan <yizh.contact@gmail.com>
2023-05-27 07:10:23 -0700

    make the snakes move altogether

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,12 +1,18 @@
 from turtle import Turtle, Screen
-
+import time
 screen = Screen()
+# screen setup
 screen.setup(width=600, height=600)
+# screen background
 screen.bgcolor("black")
+# screen title
 screen.title("Snake Game")
-
+# turn off tracer
+screen.tracer(0)
+# 3 single snake position
 positions = [(0, 0), (-20, 0), (-40, 0)]
 turtles = []
+
 for number in range(3):
     turtle = Turtle(shape="square")
     turtles.append(turtle)
@@ -17,8 +23,13 @@ for number in range(3):
 
 game_on = True
 while game_on:
+    # update the whole snake
+    screen.update()
+    # delay
+    time.sleep(0.1)
     for turtle in turtles:
-        turtle.forward(100)
+        turtle.forward(20)
+
 
 
 
