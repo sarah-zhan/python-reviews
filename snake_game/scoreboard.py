8623fa23499c17b558eece810ed0c5d1de5d9e05
sarah-zhan <yizh.contact@gmail.com>
2023-05-27 13:04:04 -0700

    clean up

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -7,6 +7,5 @@ class ScoreBoard(Turtle):
         self.color("white")
         self.setposition(0, 280)
         self.score = 0
-        # self.write(f"Score: {self.score}", move=False, align="center", font=("Arial", 11, "normal"))
         self.hideturtle()
         self.clear()
