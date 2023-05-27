b8cce173d8d850118b0ba8891ce218201c8034b7
sarah-zhan <yizh.contact@gmail.com>
2023-05-27 07:29:28 -0700

    rename variable to snake/snakes

--- snake_game/main.py
+++ snake_game/main.py
@@ -11,14 +11,14 @@ screen.title("Snake Game")
 screen.tracer(0)
 # 3 single snake position
 positions = [(0, 0), (-20, 0), (-40, 0)]
-turtles = []
+snakes = []
 
 for number in range(3):
-    turtle = Turtle(shape="square")
-    turtles.append(turtle)
-    turtle.color("white")
-    turtle.penup()
-    turtle.goto(positions[number])
+    snake = Turtle(shape="square")
+    snakes.append(snake)
+    snake.color("white")
+    snake.penup()
+    snake.goto(positions[number])
 
 
 game_on = True
@@ -27,8 +27,13 @@ while game_on:
     screen.update()
     # delay
     time.sleep(0.1)
-    for turtle in turtles:
-        turtle.forward(20)
+    for number in range(len(snakes) - 1, 0, -1):
+        # the snake replaced by
+        x = snakes[number - 1].xcor()
+        y = snakes[number - 1].ycor()
+        snakes[number].goto(x, y)
+    snakes[0].forward(20)
+
 
 
 
