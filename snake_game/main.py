b036e02151c0d956e8c6f55d9a3e22687aff38d7
sarah-zhan <yizh.contact@gmail.com>
2023-05-27 06:45:39 -0700

    create a snake

--- snake_game/main.py
+++ snake_game/main.py
@@ -5,7 +5,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-
+positions = [(0, 0), (-20, 0), (-40, 0)]
+turtles = []
+for number in range(3):
+    turtles.append(Turtle(shape="square"))
+    turtles[number].color("white")
+    turtles[number].goto(positions[number])
 
 
 
