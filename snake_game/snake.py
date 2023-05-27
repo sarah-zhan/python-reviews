ace10c385f7fcd7e3bd0d7d4b1856f2719e0086e
sarah-zhan <yizh.contact@gmail.com>
2023-05-27 09:19:31 -0700

    create Snake class

---
+++ snake_game/snake.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+
+class Snake:
+    def __init__(self):
+        self.snakes = []
+        self.create_snake()
+
+    def create_snake(self):
+        for number in POSITIONS:
+            snake = Turtle(shape="square")
+            self.snakes.append(snake)
+            snake.color("white")
+            snake.penup()
+            snake.goto(number)
+
+    def move(self):
+        for number in range(len(self.snakes) - 1, 0, -1):
+            # the snake replaced by the next snake, the last one to guide
+            x = self.snakes[number - 1].xcor()
+            y = self.snakes[number - 1].ycor()
+            self.snakes[number].goto(x, y)
+        self.snakes[0].forward(20)
