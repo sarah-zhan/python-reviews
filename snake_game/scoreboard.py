00f38ecec5fa57fe27b254f70e3869f24bb01cb9
sarah-zhan <yizh.contact@gmail.com>
2023-06-01 13:53:02 -0700

    add read and write file

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,13 +1,18 @@
 from turtle import Turtle
 
 
+
+
 class ScoreBoard(Turtle):
+
     def __init__(self):
         super().__init__()
         self.color("white")
         self.setposition(0, 270)
         self.score = 0
-        self.highest_score = 0
+        with open("data.txt", mode="r") as data:
+            data_score = data.read()
+        self.highest_score = int(data_score)
         self.hideturtle()
         self.clear()
         self.write(f"Score: {self.score} Highest Score: {self.highest_score} ",
@@ -26,11 +31,11 @@ class ScoreBoard(Turtle):
         # show the scoreboard again
         self.write(f"Score: {self.score} Highest Score: {self.highest_score} ",
                    align="center", font=("Courier", 20, "normal"))
+        with open("data.txt", mode="r+") as data:
+            data.write(str(self.highest_score))
 
     def update_highest_score(self):
-        if self.score > self.highest_score:
+        if int(float(self.score)) > int(float(self.highest_score)):
             self.highest_score = self.score
         self.score = 0
         self.update_score()
-
-
