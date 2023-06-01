de8f7eab33d8c20355d978878ef5576ef0abb085
sarah-zhan <yizh.contact@gmail.com>
2023-06-01 16:32:23 -0700

    import pandas package

--- us_states_games/main.py
+++ us_states_games/main.py
@@ -2,7 +2,17 @@
 #     data = file.readlines()
 #     print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as file:
+#     # change csv object to a list
+#     data = list(csv.reader(file))
+#     temperatures = []
+#     # loop from the second
+#     for row in data[1:]:
+#         temperatures.append(int(row[1]))  # change to int
+#     print(temperatures)
 
-with open("weather_data.csv") as file:
-    data = csv.reader(file)
+import pandas
+
+
