bc15bd40cf94494eb483b6bb77fd104831f125b3
sarah-zhan <yizh.contact@gmail.com>
2023-06-01 18:45:42 -0700

    create dataframe

--- us_states_games/main.py
+++ us_states_games/main.py
@@ -15,11 +15,19 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data)
+# data = pandas.read_csv("weather_data.csv")
+# print(data)
 # print(data["temp"])
 # print(data.to_dict())
 # print(data["temp"].mean())
 # print(data["temp"].max())
 # print(data[data.temp == data.temp.max()])
-print(data[data.day == "Monday"].temp * 9/5 + 32)
+# print(data[data.day == "Monday"].temp * 9/5 + 32)
+
+# create dataframe
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
