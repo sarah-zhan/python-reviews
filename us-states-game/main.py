c63a63a892c6a05ded4141d1b8b60897d2e9c3a3
sarah-zhan <yizh.contact@gmail.com>
2023-06-01 22:33:08 -0700

    add the photo

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,11 +1,10 @@
+import turtle
 from turtle import Turtle, Screen
 
 screen = Screen()
 screen.title("U.S.A. States Game")
-
-
-
-
+screen.addshape("blank_states_img.gif")
+turtle.shape("blank_states_img.gif")
 
 
 screen.exitonclick()
