1c2d47ea1c457f31d0e0fa0da3a39f77e436a299
sarah-zhan <yizh.contact@gmail.com>
2023-06-06 23:50:16 -0700

    add prompt

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -6,5 +6,10 @@ screen.title("U.S.A. States Game")
 screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 
+answer = screen.textinput(title="State name", prompt="Tell me a State")
+print(answer)
+
+
+
 
 screen.exitonclick()
