a6f6ae139dae5ff3c5944f890766a3b448d57698
sarah-zhan <yizh.contact@gmail.com>
2023-06-08 13:43:55 -0700

    convert answer to title case

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -6,7 +6,7 @@ screen.title("U.S.A. States Game")
 screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 
-answer = screen.textinput(title="State name", prompt="Tell me a State")
+answer = screen.textinput(title="State name", prompt="Tell me a State").title()
 print(answer)
 
 
