dd2c23ea9b8bc04b3950d7a5b1890db776b22a5d
sarah-zhan <yizh.contact@gmail.com>
2023-06-09 16:10:47 -0700

    keep track of the score

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -11,11 +11,11 @@ turtle.shape("blank_states_img.gif")
 states_data = pd.read_csv("50_states.csv")
 states_list = states_data.state.to_list()
 states = states_data.state
-
+score = 0
 game_on = True
 
 while game_on:
-    answer = screen.textinput(title="State name", prompt="Tell me a State").title()
+    answer = screen.textinput(title=f"{score}/50 States correct", prompt="Tell me a State").title()
     # check whether the answer in the states list
     if answer in states_list:
         # if it is in the list, write the name
@@ -25,6 +25,7 @@ while game_on:
         row_data = states_data[states_data.state == answer]
         pen.goto(int(row_data.x), int(row_data.y))
         pen.write(answer)
+        score += 1
 
 
 
