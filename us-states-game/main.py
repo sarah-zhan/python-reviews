5523a0b9a393b121dead53d0c96a2b91ef0e8d7f
sarah-zhan <yizh.contact@gmail.com>
2023-06-09 16:06:27 -0700

    add a loop to the game

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -7,20 +7,24 @@ screen.title("U.S.A. States Game")
 screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 
-answer = screen.textinput(title="State name", prompt="Tell me a State").title()
+
 states_data = pd.read_csv("50_states.csv")
 states_list = states_data.state.to_list()
 states = states_data.state
 
-# check whether the answer in the states list
-if answer in states_list:
-    # if it is in the list, write the name
-    pen = Turtle() # create a turtle
-    pen.hideturtle()
-    pen.penup()
-    row_data = states_data[states_data.state == answer]
-    pen.goto(int(row_data.x), int(row_data.y))
-    pen.write(answer)
+game_on = True
+
+while game_on:
+    answer = screen.textinput(title="State name", prompt="Tell me a State").title()
+    # check whether the answer in the states list
+    if answer in states_list:
+        # if it is in the list, write the name
+        pen = Turtle() # create a turtle
+        pen.hideturtle()
+        pen.penup()
+        row_data = states_data[states_data.state == answer]
+        pen.goto(int(row_data.x), int(row_data.y))
+        pen.write(answer)
 
 
 
