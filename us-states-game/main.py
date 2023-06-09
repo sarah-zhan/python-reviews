19867548b420c6eeda53176600d19faa95b7dc7d
sarah-zhan <yizh.contact@gmail.com>
2023-06-09 15:53:56 -0700

    check whether the answer is in the list, if yes, write it down

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,5 +1,6 @@
 import turtle
 from turtle import Turtle, Screen
+import pandas as pd
 
 screen = Screen()
 screen.title("U.S.A. States Game")
@@ -7,8 +8,19 @@ screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 
 answer = screen.textinput(title="State name", prompt="Tell me a State").title()
-print(answer)
+states_data = pd.read_csv("50_states.csv")
+states_list = states_data.state.to_list()
+states = states_data.state
 
+# check whether the answer in the states list
+if answer in states_list:
+    # if it is in the list, write the name
+    pen = Turtle() # create a turtle
+    pen.hideturtle()
+    pen.penup()
+    row_data = states_data[states_data.state == answer]
+    pen.goto(int(row_data.x), int(row_data.y))
+    pen.write(answer)
 
 
 
