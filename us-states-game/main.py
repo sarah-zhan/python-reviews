380fb7c4207bb2cb6f48d20982e2dd638bbfdef5
sarah-zhan <yizh.contact@gmail.com>
2023-06-09 20:45:03 -0700

    generate a csv file that are missing the names

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,6 +1,7 @@
 import turtle
 from turtle import Turtle, Screen
 import pandas as pd
+import os
 
 screen = Screen()
 screen.title("U.S.A. States Game")
@@ -13,13 +14,22 @@ states_list = states_data.state.to_list()
 states = states_data.state
 score = 0
 correct_answer = []
+
 game_on = True
 
 while game_on:
+
     answer = screen.textinput(title=f"{score}/50 States correct", prompt="Tell me a State").title()
 
     if answer == "Exit":
-        game_on = False
+        # generate the missing list
+        missing_answer = []
+        for state in states_list:
+            if state not in correct_answer:
+                missing_answer.append(state)
+        df = pd.DataFrame(missing_answer)
+        df.to_csv("states_to_learn.csv")
+        break
     # check whether the answer in the states list
     if answer in states_list and answer not in correct_answer:
         # if it is in the list, write the name
@@ -31,6 +41,7 @@ while game_on:
         pen.write(answer)
         score += 1
         correct_answer.append(answer)
+
 
 
 
