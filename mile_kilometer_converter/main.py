5b60ae00f98866c38ba49950c01254f1d2a97876
sarah-zhan <yizh.contact@gmail.com>
2023-06-12 22:41:59 -0700

    adjust the padding

--- mile_kilometer_converter/main.py
+++ mile_kilometer_converter/main.py
@@ -2,7 +2,8 @@ from tkinter import *
 
 window = Tk()
 window.title("Mile to Km Converter")
-window.minsize(width=300, height=200)
+window.minsize(width=300, height=50)
+window.config(padx=30, pady=20)
 
 # entry
 input = Entry(width=10)
@@ -11,9 +12,11 @@ input.grid(column=1, row=0)
 # label
 miles_label = Label(text="Miles")
 miles_label.grid(column=2, row=0)
+miles_label.config(padx=10)
 
 is_equal_label = Label(text="is equal to")
 is_equal_label.grid(column=0, row=1)
+is_equal_label.config(padx=10)
 
 km_label = Label(text="Km")
 km_label.grid(column=2, row=1)
@@ -24,7 +27,7 @@ result_label.grid(column=1, row=1)
 
 # button
 def button_click():
-    result_label["text"] = int(input.get()) * 1.609
+    result_label["text"] = round(int(input.get()) * 1.609, 2)
 
 
 button = Button(text="Calculate", command=button_click)
