66e99c9d1bba82bbbb10ed770ef0aa5c5abcda23
sarah-zhan <yizh.contact@gmail.com>
2023-06-13 22:54:21 -0700

    save the password info into a file when click add

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,7 +1,16 @@
 from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    with open("data.txt", mode="a") as f:
+        f.write(f"{website_input.get()} | {email_input.get()} | {password_input.get()}\n")
+    website_input.delete(0, "end")
+    email_input.delete(0, "end")
+    password_input.delete(0, "end")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,10 +35,11 @@ password_label.grid(column=0, row=3)
 # input
 website_input = Entry(width=47)
 website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
 
 email_input = Entry(width=47)
 email_input.grid(column=1, row=2, columnspan=2)
-
+email_input.insert(0, "abc@gmail.com")
 password_input = Entry(width=29)
 password_input.grid(column=1, row=3)
 
@@ -37,9 +47,7 @@ password_input.grid(column=1, row=3)
 password_button = Button(text="Generate Password", width=14, border=0)
 password_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=40, height=1, highlightthickness=0)
+add_button = Button(text="Add", width=40, height=1, highlightthickness=0, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
-
-
 
 window.mainloop()
