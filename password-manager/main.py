05b5acb0d277935c5e32eb0785005891a3bc61ee
sarah-zhan <yizh.contact@gmail.com>
2023-06-13 21:51:48 -0700

    create 3 labels

--- password-manager/main.py
+++ password-manager/main.py
@@ -11,7 +11,17 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=img)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
+
+# label
+website_lable = Label(text="Website: ")
+website_lable.grid(column=0, row=1)
+
+email_label = Label(text="Email/Username: ")
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password: ")
+password_label.grid(column=0, row=3)
 
 
 
