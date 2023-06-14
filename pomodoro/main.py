016567b5c56ec21fc08cca76e9aa6889eed6daad
sarah-zhan <yizh.contact@gmail.com>
2023-06-13 20:38:39 -0700

    add contions to change the timer session

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,20 +1,39 @@
 import math
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 2
+LONG_BREAK_MIN = 3
+reps = 0
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start():
-    count_down(8*60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_sec = SHORT_BREAK_MIN * 60
+    long_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_sec)
+        timer_label.config(text="Break", fg=PINK)
+    elif reps % 2 != 0:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(num):
     minute = math.floor(num / 60)
@@ -25,21 +44,21 @@ def count_down(num):
         minute = f"0{minute}"
     canvas.itemconfig(clock_text, text=f"{minute}:{second}")
     if num > 0:
-        window.after(1000, count_down, num - 1)
+        window.after(100, count_down, num - 1)
+    else:
+        start()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=40, bg=YELLOW)
 
-
-
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 clock_text = canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
 canvas.grid(column=1, row=1)
-
 
 # label
 timer_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"), bg=YELLOW, fg=GREEN)
@@ -52,7 +71,5 @@ start_button = Button(text="Start", border=0.5, command=start)
 start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", border=0.5)
 reset_button.grid(column=2, row=2)
-
-
 
 window.mainloop()
