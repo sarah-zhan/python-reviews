395e6089b50fdd8a4f8a65d0dbc77ea1aea43758
sarah-zhan <yizh.contact@gmail.com>
2023-08-26 17:57:56 -0700

    use create text for the words

--- flash-card/main.py
+++ flash-card/main.py
@@ -15,6 +15,8 @@ img_right = PhotoImage(file="./images/right.png")
 img_wrong = PhotoImage(file="./images/wrong.png")
 
 canvas.create_image(400, 263, image=img_front)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0)
 
@@ -25,11 +27,7 @@ button_right.grid(column=1, row=2)
 button_wrong = Button(image=img_wrong, highlightthickness=0)
 button_wrong.grid(column=0, row=2)
 
-#label
-french = Label(text="French", font=("Ariel", 40, "italic"), background="white")
-french.place(x=300, y=150)
-french_word = Label(text="trouve", font=("Ariel", 60, "bold"), background="white")
-french_word.place(x=260, y=263)
+
 
 
 window.mainloop()
