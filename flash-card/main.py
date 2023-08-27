c2679f4679d9238a56b4ae2567da21f53fd70ced
sarah-zhan <yizh.contact@gmail.com>
2023-08-26 23:15:32 -0700

    convert the data to dictionary list

--- flash-card/main.py
+++ flash-card/main.py
@@ -1,6 +1,13 @@
 from tkinter import *
+import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pd.read_csv("./data/french_words.csv")
+data_dictionary_list = data.to_dict(orient='records')
+print(data_dictionary_list)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
